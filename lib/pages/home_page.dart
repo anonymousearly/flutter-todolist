9d9e6105496aaf8a27cdc18270f1d6b3435a9581
Earl Dave <elaco.earl13@gmail.com>
2025-05-07 08:40:11 +0800

    Updated features

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,21 +8,42 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {  
+class _HomePageState extends State<HomePage> {
+  //list of tasks
+  List toDoList = [
+    ["Naruto", false], 
+    ["Naruto: Shippuden", false]
+  ];
+
+  //check box was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[200],
       appBar: AppBar(
-        title: Text('My Todo List'),
-        backgroundColor: Colors.blue,
+        title: Text('My Todo List'), backgroundColor: Colors.blue,
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ) ,
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
-    );      
+    );
   }
 }
